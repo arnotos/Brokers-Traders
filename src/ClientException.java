12dbb2b5c63ea0950ecf21c6c39c8e67d95edb6f
arnotos <lapenna.arnaud@gmail.com>
2018-02-27 11:42:14 +0100

    javadoc et menage

--- src/ClientException.java
+++ src/ClientException.java
@@ -1,5 +1,12 @@
-
-
+/**
+ * 
+ * @author arnaud lapenna
+ *
+ * @info custom exeption classes for error handling
+ * 
+ * Exception class for the traders
+ * 
+ */
 public class ClientException extends Exception {
 	
 	/**
@@ -7,17 +14,32 @@ public class ClientException extends Exception {
 	 */
 	private static final long serialVersionUID = -7065268619672827899L;
 	
+	//Custom message to identify the error and have a clear message of the incoming error
 	String messageUser="";
 
+	/**
+	 * Basic constructor who print just 'ERREUR CLIENT' when instantiated 
+	 * 
+	 */
 	public ClientException(){
 		System.out.println("ERREUR CLIENT");
 	}
 	
+	/**
+	 * Advanced constructor of ClientException with a custom message
+	 * 
+	 * @param messagePerso
+	 * 			the custom message describing the new error 
+	 * @info set the custom message and print it immediately. 
+	 */
 	public ClientException(String messagePerso){
 		messageUser=messagePerso;
 		System.out.println(messageUser);		
 	}
 	
+	/**
+	 * Print the register message into the console
+	 */
 	public void showErrorMessage(){
 		System.out.println(messageUser);
 	}
--- src/ServerException.java
+++ src/ServerException.java
@@ -1,5 +1,12 @@
-
-
+/**
+ * 
+ * @author arnaud lapenna
+ *
+ * @info custom exeption classes for error handling
+ * 
+ * Exeption class for the broker
+ * 
+ */
 public class ServerException extends Exception {
 	
 	/**
@@ -7,17 +14,32 @@ public class ServerException extends Exception {
 	 */
 	private static final long serialVersionUID = -1754766737955662949L;
 	
+	//Custom message to identify the error and have a clear message of the incoming error
 	String messageServer="";
 
+	/**
+	 * Basic constructor who print just 'ERREUR SERVER' when instantiated 
+	 * 
+	 */
 	public ServerException(){
 		System.out.println("ERREUR SERVER");
 	}
 	
+	/**
+	 * Advanced constructor of ServerException with a custom message
+	 * 
+	 * @param messagePerso
+	 * 			the custom message describing the new error 
+	 * @info set the custom message and print it immediately. 
+	 */
 	public ServerException(String messagePerso){
 		messageServer=messagePerso;
 		System.out.println(messageServer);		
 	}
 	
+	/**
+	 * Print the register message into the console
+	 */
 	public void showErrorMessage(){
 		System.out.println(messageServer);
 	}
